35e29325081fdf581edeb7e6445c757d1c3b90f6
Jordyn O Cleirgh <jchuhaloff@gmail.com>
2026-02-06 14:07:49 -0800

    Simplify SendVerificationCode

--- src/components/SendVerificationCode.tsx
+++ src/components/SendVerificationCode.tsx
@@ -1,7 +1,6 @@
-import { useCallback, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Trans } from "@lingui/react/macro";
-import { debounce } from "lodash";
+import { Trans } from "@lingui/macro";
 
 import { SEND_VERIFICATION_CODE } from "ducks/firebase";
 import { setPage } from "ducks/page";
@@ -11,34 +10,16 @@ import { Page } from "types/Page";
 import { type State } from "types/State";
 import { useAppDispatch } from "hooks/useAppDispatch";
 
-const DELAY = 3000;
-
 export function SendVerificationCode() {
   const appEnv = (window as any).APP_ENV;
 
   const dispatch = useAppDispatch();
   const sendCodeStatus = useStatus(SEND_VERIFICATION_CODE);
-  const [isLoading, setIsLoading] = useState(true);
   const phoneNumber = useSelector((state: State) => state.phoneNumber ?? "");
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const sendVerificationCodeDebounce = useCallback(
-    debounce(() => {
-      if (phoneNumber) {
-        sendVerificationCode({ phoneNumber, dispatch });
-      }
-    }, 1000),
-    [phoneNumber, dispatch],
-  );
-
   const handleSendVerification = () => {
-    sendVerificationCodeDebounce();
+    sendVerificationCode({ phoneNumber, dispatch });
   };
-
-  useEffect(() => {
-    handleSendVerification();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   useEffect(() => {
     if (sendCodeStatus.isSuccess) {
@@ -46,44 +27,28 @@ export function SendVerificationCode() {
     }
   }, [sendCodeStatus.isSuccess, dispatch]);
 
-  // We automatically trigger the recaptcha verification. To avoid flashing the
-  // "Send verification code" button, we display the button when DELAY has
-  // passed.
-  useEffect(() => {
-    const tid = setTimeout(() => {
-      setIsLoading(false);
-    }, DELAY);
-
-    return () => clearTimeout(tid);
-  }, []);
-
   return (
     <div className="panel">
-      {isLoading && (
-        <div className="text-center">
-          <p>
+      <button
+        className="button"
+        style={{ backgroundColor: appEnv.BUTTON_COLOR }}
+        onClick={handleSendVerification}
+        disabled={
+          !phoneNumber || sendCodeStatus.isLoading || sendCodeStatus.isSuccess
+        }
+      >
+        <span>
+          {sendCodeStatus.isLoading || sendCodeStatus.isSuccess ? (
             <Trans>Please wait…</Trans>
-          </p>
-        </div>
-      )}
+          ) : (
+            <Trans>Send verification code</Trans>
+          )}
+        </span>
+      </button>
 
       {sendCodeStatus.error && (
         <p>
           <Trans>Error: {sendCodeStatus.error.message}</Trans>
-        </p>
-      )}
-
-      {!isLoading && !sendCodeStatus.error && (
-        <p className="text-center">
-          <button
-            className="button"
-            style={{ backgroundColor: appEnv.BUTTON_COLOR }}
-            onClick={handleSendVerification}
-          >
-            <span>
-              <Trans>Send verification code</Trans>
-            </span>
-          </button>
         </p>
       )}
     </div>
